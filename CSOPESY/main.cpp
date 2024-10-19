5cc9751f1ce0f0099c83f3210f304ce223f62c1d
Jean <jeancabrera2003@gmail.com>
2024-10-19 16:57:20 +0800

    added diff scheduling

---
+++ CSOPESY/main.cpp
@@ -0,0 +1,370 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <windows.h>
+#include <sstream>
+#include <ctime>
+#include <cstdio>
+#include <mutex>
+#include <map>
+#include <iomanip>
+#include <atomic>
+#include <thread>
+#include <fstream>
+#include <queue>
+#include <memory>
+#include <fstream>
+#include <filesystem>
+
+#include "screen.h"
+#include "scheduler.h"
+
+using namespace std;
+typedef long long ll;
+
+class MainConsole : public abstract_screen
+{
+private:
+	string currentView = "MainMenu";
+	bool continue_program = true;
+	map<string, shared_ptr<Screen>> screenList;
+	Scheduler scheduler;
+	atomic<bool> scheduleBool = true;
+	bool schedulerRunning = false;
+	int schedulerCtr = 0;
+
+	void commandNotRecognize(string command_to_check)
+	{
+		write("Unknown command: " + command_to_check);
+	}
+
+	void invalidCommand(string command_to_check)
+	{
+		write("Invalid Arguments for " + command_to_check);
+	}
+
+	void screenNotFound(string name)
+	{
+		write("Process " + name + " not found.");
+	}
+
+	bool mainMenuCommand(vector<string> seperatedCommand, string command_to_check)
+	{
+		const set<string> commands = {"initialize", "screen", "scheduler-test", "scheduler-stop", "report-util", "clear", "exit"};
+
+		if (!commands.count(seperatedCommand[0]))
+		{
+			commandNotRecognize(command_to_check);
+			return true;
+		}
+
+		if (seperatedCommand[0] == "exit")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			return false;
+		}
+
+		if (seperatedCommand[0] != "initialize" && !scheduler.isInitialized())
+		{
+			write("Please initialize the configuration first.");
+			return true;
+		}
+
+		if (seperatedCommand[0] == "initialize")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			scheduler.getConfig();
+			write("Configuration initialized.");
+		}
+
+		else if (seperatedCommand[0] == "screen")
+		{
+			if (seperatedCommand.size() < 2 || seperatedCommand.size() > 3)
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+
+			if (seperatedCommand.size() == 2)
+			{
+				if (!(seperatedCommand[1] == "-ls"))
+				{
+					invalidCommand(command_to_check);
+					return true;
+				}
+				write("CPU utilization: " + scheduler.getCpuUtilization());
+				write("Cores used: " + to_string(scheduler.getCoresUsed()));
+				write("Cores available: " + to_string(scheduler.getCoresAvail()));
+				write("");
+				write("--------------------------------------");
+				write("Running processes:");
+				vector<shared_ptr<Screen>> processingScreens;
+				vector<shared_ptr<Screen>> finishedScreens;
+				for (auto [_, sc] : screenList)
+				{
+					if (sc->isFinished())
+					{
+						finishedScreens.push_back(sc);
+					}
+					else
+					{
+						processingScreens.push_back(sc);
+					}
+				}
+				for (auto sc : processingScreens)
+				{
+					write(sc->listProcess());
+				}
+				write("\nFinished processes:");
+				for (auto sc : finishedScreens)
+				{
+					write(sc->listProcess());
+				}
+				write("--------------------------------------");
+			}
+
+			else if (seperatedCommand[1] == "-r")
+			{
+				if (!screenList.count(seperatedCommand[2]))
+				{
+					screenNotFound(seperatedCommand[2]);
+					return true;
+				}
+				currentView = screenList[seperatedCommand[2]]->getProcessName();
+				if (screenList[seperatedCommand[2]]->isFinished())
+				{
+					write("This Process is already finished.");
+					return true;
+				}
+				if (screenList[seperatedCommand[2]]->isInitialized())
+				{
+					screenList[seperatedCommand[2]]->redraw();
+					processCommand("process-smi");
+					screenList[seperatedCommand[2]]->add("root:\\> process-smi");
+				}
+				else
+				{
+					screenList[seperatedCommand[2]]->openScreen();
+				}
+			}
+			else if (seperatedCommand[1] == "-s")
+			{
+				if (screenList.count(seperatedCommand[2]))
+				{
+					write("This Process is already in use.");
+					return true;
+				}
+				auto sc = make_shared<Screen>(seperatedCommand[2], scheduler.getMinIns(), scheduler.getMaxIns());
+				screenList[seperatedCommand[2]] = sc;
+				screenList[seperatedCommand[2]]->openScreen();
+				screenList[seperatedCommand[2]]->initialize();
+				currentView = screenList[seperatedCommand[2]]->getProcessName();
+				scheduler.pushQueue(sc);
+			}
+			else
+			{
+				invalidCommand(command_to_check);
+			}
+		}
+		else if (seperatedCommand[0] == "clear")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			system("cls");
+			this->buffer.clear();
+			print_header();
+		}
+		else if (seperatedCommand[0] == "scheduler-test")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			if (schedulerRunning)
+			{
+				write("Scheduler is already running.");
+			}
+			else
+			{
+				thread testThread(&MainConsole::schedulerTest, this);
+				testThread.detach();
+				schedulerRunning = true;
+				write("Scheduler Test started.");
+			}
+		}
+		else if (seperatedCommand[0] == "scheduler-stop")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			scheduleBool = false;
+			schedulerRunning = false;
+			write("Scheduler Test stopped.");
+		}
+		else if (seperatedCommand[0] == "report-util")
+		{
+			if (!(seperatedCommand.size() == 1))
+			{
+				invalidCommand(command_to_check);
+				return true;
+			}
+			filesystem::path currentPath = filesystem::current_path();
+			string outputFileName = (currentPath / "csopesy-log.txt").string();
+			ofstream outFile(outputFileName);
+
+			outFile << "CPU utilization: " << scheduler.getCpuUtilization() << endl;
+			outFile << "Cores used: " << scheduler.getCoresUsed() << endl;
+			outFile << "Cores available: " << scheduler.getCoresAvail() << endl;
+			outFile << endl;
+			outFile << "--------------------------------------" << endl;
+			outFile << "Running processes:" << endl;
+			vector<shared_ptr<Screen>> processingScreens;
+			vector<shared_ptr<Screen>> finishedScreens;
+			for (auto [_, sc] : screenList)
+			{
+				if (sc->isFinished())
+				{
+					finishedScreens.push_back(sc);
+				}
+				else
+				{
+					processingScreens.push_back(sc);
+				}
+			}
+			for (auto sc : processingScreens)
+			{
+				outFile << sc->listProcess() << endl;
+			}
+			outFile << endl
+					<< "Finished processes:" << endl;
+			for (auto sc : finishedScreens)
+			{
+				outFile << sc->listProcess() << endl;
+			}
+			outFile << "--------------------------------------";
+			outFile.close();
+			write("Report generated at " + outputFileName);
+		}
+
+		return true;
+	}
+
+	void schedulerTest()
+	{
+		ll freq = scheduler.getBatchProcessFrequency();
+		while (scheduleBool)
+		{
+			for (ll i = 0; i < freq; i++)
+			{
+				continue;
+			}
+			string processName;
+			while (true)
+			{
+				processName = "p" + to_string(schedulerCtr);
+				if (!screenList.count(processName))
+				{
+					break;
+				}
+				schedulerCtr++;
+			}
+			auto sc = make_shared<Screen>(processName, scheduler.getMinIns(), scheduler.getMaxIns());
+			screenList[processName] = sc;
+			scheduler.pushQueue(sc);
+		}
+	}
+
+	bool processCommand(const string &command)
+	{
+		string command_to_check = command;
+		transform(command_to_check.begin(), command_to_check.end(), command_to_check.begin(), ::tolower);
+
+		stringstream stream(command);
+		vector<string> seperatedCommand;
+
+		string splitter;
+		while (stream >> splitter)
+		{
+			seperatedCommand.push_back(splitter);
+		}
+
+		if (currentView == "MainMenu")
+		{
+			return mainMenuCommand(seperatedCommand, command_to_check);
+		}
+		else
+		{
+			if (screenList[currentView]->screenCommand(seperatedCommand, command_to_check))
+			{
+				currentView = "MainMenu";
+				this->redraw();
+			}
+		}
+		return true;
+	}
+
+	void setConsoleColor(int color)
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, color);
+	}
+
+	void print_header()
+	{
+		write("   _____  _____  ____  _____  ______  _______     __");
+		write("  / ____|/ ____|/ __ \\|  __ \\|  ____|/ ____\\ \\   / /");
+		write(" | |    | (___ | |  | | |__) | |__  | (___  \\ \\_/ /");
+		write(" | |     \\___ \\| |  | |  ___/|  __|  \\___ \\  \\   /");
+		write(" | |____ ____) | |__| | |    | |____ ____) |  | |");
+		write("  \\_____|_____/ \\____/|_|    |______|_____/   |_|");
+		write("Hello, Welcome to CSOPESY commandline!", 2);
+		write("Type 'exit' to quit, 'clear' to clear the screen", 14);
+	}
+
+public:
+	void run()
+	{
+		string user_input;
+
+		continue_program = true;
+		print_header();
+
+		while (continue_program)
+		{
+			cout << "root:\\> ";
+			getline(cin, user_input);
+			if (currentView != "MainMenu")
+			{
+				screenList[currentView]->add("root:\\> " + user_input);
+			}
+			else
+			{
+				add("root:\\> " + user_input);
+			}
+			continue_program = processCommand(user_input);
+		}
+	}
+};
+
+signed main()
+{
+	MainConsole console;
+	console.run();
+	return 0;
+}
---
+++ CSOPESY/screen.h
@@ -0,0 +1,245 @@
+#pragma once
+
+using namespace std;
+typedef long long ll;
+
+struct BufferEntry
+{
+	string text;
+	WORD color;
+};
+
+class abstract_screen
+{
+protected:
+	vector<BufferEntry> buffer;
+	mutex console_mutex;
+
+	void write(const string &text, WORD color = 7)
+	{
+		lock_guard<mutex> lock(console_mutex);
+		buffer.push_back({text, color});
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+		cout << text << endl;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	}
+
+public:
+	virtual void redraw()
+	{
+		system("cls");
+		lock_guard<mutex> lock(console_mutex);
+		for (const auto &entry : buffer)
+		{
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), entry.color);
+			cout << entry.text << endl;
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+		}
+	};
+	virtual void add(const string &text, WORD color = 7)
+	{
+		lock_guard<mutex> lock(console_mutex);
+		buffer.push_back({text, color});
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	}
+};
+
+class Screen : public abstract_screen
+{
+private:
+	string processName;
+	int currentLine;
+	int totalLine;
+	time_t timestamp;
+	bool finished = false;
+	bool initialized = false;
+	int core_id = -1;
+
+	stringstream
+	printScreen_helper()
+	{
+		stringstream ss;
+		char buffer[26];
+		ctime_s(buffer, sizeof(buffer), &timestamp);
+		ss << "Process: " << processName << endl;
+		if (finished)
+		{
+			ss << "Finished!" << endl;
+		}
+		else
+		{
+			ss << "Current Line: " << currentLine << " / " << totalLine << endl;
+		}
+		ss << "Timestamp: " << buffer;
+
+		return ss;
+	}
+
+public:
+	Screen(std::string name, int min, int max)
+		: processName(name), currentLine(1), timestamp(time(nullptr))
+	{
+		srand(static_cast<unsigned int>(time(nullptr)));
+		totalLine = min + rand() % ((max + 1) - min);
+	}
+
+	Screen(const Screen &other)
+	{
+		processName = other.processName;
+		currentLine = other.currentLine;
+		totalLine = other.totalLine;
+		timestamp = other.timestamp;
+	}
+
+	Screen &operator=(const Screen &other)
+	{
+		if (this == &other)
+			return *this;
+		processName = other.processName;
+		currentLine = other.currentLine;
+		totalLine = other.totalLine;
+		timestamp = other.timestamp;
+		buffer = other.buffer;
+		return *this;
+	}
+
+	Screen()
+	{
+	}
+	void setCoreId(int id)
+	{
+		core_id = id;
+	}
+	int getCoreId() const
+	{
+		return core_id;
+	}
+	string convert_unix_to_string(time_t unix_timestamp) {
+		struct tm time_info; 
+
+		localtime_s(&time_info, &unix_timestamp); 
+
+		char time_string[100];
+		strftime(time_string, sizeof(time_string), "(%m/%d/%Y %I:%M:%S %p)", &time_info); 
+		return string(time_string); 
+	}
+	string listProcess()
+	{
+		stringstream ss;
+		if (isFinished())
+		{
+			ss << left << setw(20) << getProcessName() << setw(2) << "" << setw(23) << convert_unix_to_string(getTimestamp()) << setw(4) << "" << "Finished" << setw(5) << "" << getCurrentLine() << " / " << getTotalLine();
+		}
+		else
+		{
+			ss << left << setw(20) << getProcessName() << setw(2) << "" << setw(23) << convert_unix_to_string(getTimestamp()) << setw(4) << "" << "Core: " << getCoreId() << setw(5) << "" << getCurrentLine() << " / " << getTotalLine();
+		}
+		return ss.str();
+	}
+
+	string getProcessName() const
+	{
+		return processName;
+	}
+
+	int getCurrentLine() const
+	{
+		return currentLine;
+	}
+
+	int getTotalLine() const
+	{
+		return totalLine;
+	}
+
+	time_t getTimestamp() const
+	{
+		return timestamp;
+	}
+
+	bool operator<(const Screen &other) const
+	{
+		return timestamp < other.timestamp;
+	}
+
+	bool isInitialized()
+	{
+		return initialized;
+	}
+
+	void initialize()
+	{
+		initialized = true;
+	}
+
+	void execute()
+	{
+		currentLine++;
+		if (currentLine == totalLine)
+		{
+			finished = true;
+		}
+	}
+
+	void openScreen()
+	{
+		system("cls");
+		screenInfo();
+	}
+
+	bool isFinished()
+	{
+		return finished;
+	}
+	void screenInfo()
+	{
+		string content = printScreen_helper().str();
+		int width = content.length() + 4;
+
+		write("+" + string(width - 2, '-') + "+");
+
+		stringstream contentStream(content);
+		string line;
+		while (getline(contentStream, line))
+		{
+			write("| " + line + string(width - line.length() - 4, ' ') + " |");
+		}
+		write("+" + string(width - 2, '-') + "+");
+	}
+
+	bool screenCommand(vector<string> seperatedCommand, string command_to_check)
+	{
+
+		const set<string> commands = {"exit", "process-smi"};
+
+		if (!commands.count(seperatedCommand[0]))
+		{
+			write("Unknown command: " + command_to_check);
+			return false;
+		}
+
+		if (seperatedCommand[0] == "exit")
+		{
+			if (seperatedCommand.size() != 1)
+			{
+				write("INVALID COMMAND");
+				return false;
+			}
+			return true;
+		}
+
+		if (seperatedCommand[0] == "process-smi")
+		{
+			if (seperatedCommand.size() != 1)
+			{
+				write("INVALID COMMAND");
+				return false;
+			}
+			write("");
+			screenInfo();
+			return false;
+		}
+		return false;
+	}
+};
